7595bf3e5125029968aa8c6521bfc7610b1d59a4
SeiEinFisch <hoangnam2001@ymail.com>
2024-03-26 13:14:35 +0100

    Git setup

---
+++ hosts/default/home.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../../modules/home-manager
+  ];
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "sei";
+  home.homeDirectory = "/home/sei";
+
+  
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # "Hello, world!" when run.
+    pkgs.hello
+
+  ];
+
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
---
+++ modules/home-manager/git/default.nix
@@ -0,0 +1,9 @@
+{...}:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "SeiEinFisch";
+    userEmail = "hoangnam2001@ymail.com";
+    };
+}
