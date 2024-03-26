cfbfa5c39db53ce9eed742cebae29729020451e4
SeiEinFisch <hoangnam2001@ymail.com>
2024-03-26 15:33:03 +0100

    Discord and spotify added

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -117,6 +117,9 @@
      gedit
      wget
      kitty
+     discord
+     spotify
+     
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
