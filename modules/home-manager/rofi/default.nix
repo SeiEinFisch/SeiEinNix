fc4ef1a569a8dbfee2e1e536ba436c8bd39ceb31
SeiEinFisch <hoangnam2001@ymail.com>
2024-03-27 11:05:24 +0100

    rofi

---
+++ modules/home-manager/rofi/default.nix
@@ -0,0 +1,20 @@
+{lib, config, pkgs,...}:
+{
+   programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland;
+    extraConfig = {
+        modi = "run,drun,filebrowser";
+        icon-theme = "Oranchelo";
+        show-icons = true;
+        drun-display-format = "{icon} {name}";
+        location = 0;
+        hide-scrollbar = true;
+        display-drun = "Apps";
+        display-run = "Run";
+        display-filebrowser = " FileBrowser";
+        sidebar-mode = true;
+    };
+   }
+
+}
