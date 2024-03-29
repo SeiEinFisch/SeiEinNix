981f2af3ad78b77763ce97e3b21bb7742574927b
SeiEinFisch <hoangnam2001@ymail.com>
2024-03-29 21:26:51 +0100

    kitty theming(doesnt work)

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -2,11 +2,11 @@
 {   
     programs.kitty.enable = true;
     programs.kitty.settings = {
-        colors = with; config.colorScheme.colors; {
-            foreground = "0x${base06}";
-            background = "0x${base00}";
-            background_opacity = "0.8";
+        
+        #foreground = "0x${config.colorScheme.colors.base06}";        
+        #background = "0x${config.colorScheme.colors.base00}";
+        background_opacity = "0.8";
+    
             
-        }
-    }
+    };
 }
